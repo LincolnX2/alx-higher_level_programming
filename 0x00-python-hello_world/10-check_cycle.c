69d2a1df14723846e51b207c11c62d6da2568eb9
LincolnX2 <sabur.yinus@gmail.com>
2022-11-29 04:03:09 -0800

    python3

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,33 +1,22 @@
 #include "lists.h"
 
 /**
-
  * check_cycle - checks for cycle in a linked list
-
  * @list: linked list to check
-
  * Return: 1 if cycle, 0 if not
-
- */
+ *
 
 int check_cycle(listint_t *list)
-
 {
-
 	listint_t *temp1 = NULL, *temp2 = NULL;
-
-
 
 	temp1 = list;
 
 	temp2 = list;
 
-
-
 	while (list)
 
 	{
-
 		temp2 = temp2->next;
 
 		if (!temp1 || !temp2)
@@ -41,5 +30,4 @@ int check_cycle(listint_t *list)
 	}
 
 	return (0);
-
 }
