9c06a7439ead0ca0c4e31874a9ae8921be7a6995
https://github.com/LincolnX2 <sabur.yinus@gmail.com>
2023-03-14 15:25:06 -0700

    JS csript

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -2,7 +2,7 @@
 #!/usr/bin/node
 // JS Script
 
-let arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let arr = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (let i = 0; i < arr.length; i++) {
   console.log(arr[i]);
 }
