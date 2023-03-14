61ec9e40b73e405cf42226b9cf6bc9bc83bda9a9
https://github.com/LincolnX2 <sabur.yinus@gmail.com>
2023-03-14 14:56:42 -0700

    js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-// JS Script
+// JS script
 
-console.log(“C is fun”)
-console.log(“Python is cool”)
-console.log(“JavaScript is amazing”)
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
